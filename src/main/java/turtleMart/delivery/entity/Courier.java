9f229ba3a43ab4cb8b81e35d810c0e01b8da3a3d
tmdcksdl <tmdcksdl99@naver.com>
2025-05-21 09:46:05 +0900

    add: 택배사 엔티티 생성

---
+++ src/main/java/turtleMart/delivery/entity/Courier.java
@@ -0,0 +1,33 @@
+package turtleMart.delivery.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Courier {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String code;
+
+    private String trackingUrlTemplate;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
