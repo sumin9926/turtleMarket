4c0be2492f53f903367343a7cfb40d7c9dabffc5
Happy Beny <qkdehdgur3@gmail.com>
2025-05-22 08:56:35 +0900

    feat : 결제 조회(개인단건/개인전체/관리자용) 메서드 추가

--- src/main/java/turtleMart/payment/repository/PaymentRepository.java
+++ src/main/java/turtleMart/payment/repository/PaymentRepository.java
@@ -3,5 +3,9 @@ package turtleMart.payment.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import turtleMart.payment.entity.Payment;
 
-public interface PaymentRepository extends JpaRepository<Payment, Integer> {
+import java.util.List;
+import java.util.Optional;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Optional<List<Payment>> findAllByMemberId(Long memberId);
 }
--- src/main/java/turtleMart/payment/service/PaymentService.java
+++ src/main/java/turtleMart/payment/service/PaymentService.java
@@ -11,6 +11,8 @@ import turtleMart.payment.dto.response.PaymentResponse;
 import turtleMart.payment.entity.Payment;
 import turtleMart.payment.repository.PaymentRepository;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -38,5 +40,53 @@ public class PaymentService {
         paymentRepository.save(payment);
 
         return PaymentResponse.from(payment);
+    }
+
+    public List<PaymentResponse> getMyAllPayments(Long memberId) {
+        List<Payment> payments = paymentRepository.findAllByMemberId(memberId).orElseThrow(
+                () -> new RuntimeException("")
+        );
+
+        return payments.stream().map(PaymentResponse::from).toList();
+    }
+
+    public PaymentResponse getMyPayment(Long paymentId, Long memberId) {
+        Payment foundPayment = findPayment(paymentId);
+        checkOwnPayment(foundPayment, memberId);
+        return PaymentResponse.from(foundPayment);
+    }
+
+    public List<PaymentResponse> getAllPayments(Long memberId) {
+        checkAdmin(memberId);
+        List<Payment> payments;
+        if (memberId != null) {
+            payments = paymentRepository.findAllByMemberId(memberId).orElseThrow(
+                    () -> new RuntimeException("")
+            );
+        } else {
+            payments = paymentRepository.findAll();
+        }
+
+        return payments.stream().map(PaymentResponse::from).toList();
+    }
+
+
+
+    private Payment findPayment(Long paymentId) {
+        return paymentRepository.findById(paymentId).orElseThrow(
+                () -> new RuntimeException("")
+        );
+    }
+
+    private void checkOwnPayment(Payment payment, Long memberId) {
+        if (!payment.getMember().getId().equals(memberId)) {
+            throw new RuntimeException("");
+        }
+    }
+
+    private void checkAdmin(Long loggedInId) {
+//        if (!foundMember.getUserRole().equals(UserRole.ADMIN)) {
+//            throw new RuntimeException("");
+//        }
     }
 }
