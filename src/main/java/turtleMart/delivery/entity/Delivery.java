e2441ba81e495819067643defe10de7269da2100
tmdcksdl <tmdcksdl99@naver.com>
2025-05-21 10:01:35 +0900

    feat: 배송 엔티티 수정
    - 수령인 이름, 전화번호 컬럼 추가
    - 주소, 상세 주소 컬럼 추가
    - 생성일, 수정일 컬럼 추가
    - 연관관계 컬럼 설정

--- src/main/java/turtleMart/delivery/entity/Delivery.java
+++ src/main/java/turtleMart/delivery/entity/Delivery.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import turtleMart.member.entity.Address;
 import turtleMart.member.entity.Seller;
 import turtleMart.order.entity.Order;
@@ -20,26 +22,36 @@ public class Delivery {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
     private Order order;
 
-    @ManyToOne
-    @JoinColumn
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "seller_id", nullable = false)
     private Seller seller;
 
-    @ManyToOne
-    @JoinColumn
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "address_id", nullable = false)
     private Address address;
 
-    private DeliveryStatus deliveryStatus;
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus deliveryStatus = DeliveryStatus.PREPARING;
 
-    private String address2;
+    private String saveAddress;
 
-    private String addressDetail;
+    private String saveAddressDetail;
+
+    private String receiverName;
+
+    private String receiverPhone;
 
     private LocalDateTime shippedAt;
 
     private LocalDateTime deliveredAt;
 
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
