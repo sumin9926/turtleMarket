79749b2950d60c4b7021887d52d0a55d675c86ff
임PRO스터\su992 <su9926@naver.com>
2025-05-23 01:23:18 +0900

    feat: CartOrderSheetRequest에 URL 파라미터 파싱을 위한 메서드 구현
    - OrderSheetController에 해당 메서드 적용

--- src/main/java/turtleMart/order/dto/request/CartOrderSheetRequest.java
+++ src/main/java/turtleMart/order/dto/request/CartOrderSheetRequest.java
@@ -3,6 +3,9 @@ package turtleMart.order.dto.request;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
+import java.util.Arrays;
+import java.util.List;
+
 public record CartOrderSheetRequest(
         @NotNull(message = "상품 ID는 필수입니다.")
         Long productId,
@@ -10,4 +13,14 @@ public record CartOrderSheetRequest(
         @Min(value = 1, message = "수량은 1 이상이어야 합니다.")
         Integer quantity
 ) {
+    public static List<CartOrderSheetRequest> splitItemNameAndQuantity(String items) {
+        return Arrays.stream(items.split(","))
+                .map(entry -> {
+                    String[] parts = entry.split(":");
+                    Long productId = Long.parseLong(parts[0]);
+                    Integer quantity = Integer.parseInt(parts[1]);
+                    return new CartOrderSheetRequest(productId, quantity);
+                })
+                .toList();
+    }
 }
