7f1a4b6b98ec19c8b87341006a83064b9ed9fcf2
임PRO스터\su992 <su9926@naver.com>
2025-05-23 09:05:23 +0900

    feat: Order, OrderItem 엔티티 기본 생성자 및 정적 팩토리 메서드 추가
    - Order 엔티티에 편의 메서드, totalPrice calculate 메서드 추가됨
    - Order 엔티티의 orderAt 필드명 변경(orderAt → orderedAt)

--- src/main/java/turtleMart/order/entity/OrderItem.java
+++ src/main/java/turtleMart/order/entity/OrderItem.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import turtleMart.product.entity.Product;
 
 @Entity
@@ -15,6 +16,7 @@ public class OrderItem {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Setter
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
@@ -31,7 +33,20 @@ public class OrderItem {
 
     private OrderItemStatus orderItemStatus;
 
+    private OrderItem(Order order, Product product, Integer price, String name, Integer quantity, OrderItemStatus orderItemStatus) {
+        this.order = order;
+        this.product = product;
+        this.price = price;
+        this.name = name;
+        this.quantity = quantity;
+        this.orderItemStatus = orderItemStatus;
+    }
+
+    public static OrderItem of(Order order, Product product, Integer price, String name, Integer quantity) {
+        return new OrderItem(order, product, price, name, quantity, OrderItemStatus.UNPAID);
+    }
+
     public void updateStatus(OrderItemStatus status) {
-        this.orderItemStatus=status;
+        this.orderItemStatus = status;
     }
 }
