faeb046a4a63039fdb3979f3bbac0983bf662ae3
임PRO스터\su992 <su9926@naver.com>
2025-05-23 09:08:07 +0900

    feat: PaymentRequest 정적 팩토리 메서드 추가 및 어노테이션 수정

--- src/main/java/turtleMart/payment/dto/request/PaymentRequest.java
+++ src/main/java/turtleMart/payment/dto/request/PaymentRequest.java
@@ -1,15 +1,28 @@
 package turtleMart.payment.dto.request;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotNull;
+import turtleMart.order.dto.request.OrderRequest;
+import turtleMart.order.entity.Order;
 import turtleMart.payment.entity.PaymentMethod;
 
 public record PaymentRequest(
-        @NotBlank Long orderId,
-        @NotBlank Long memberId,
-        @NotBlank @Size(min = 100, message = "100원 이상 결제하셔야 합니다.") int amount,
-        @NotBlank PaymentMethod paymentMethod,
+        @NotNull Long orderId,
+        @NotNull Long memberId,
+        @Min(value = 100, message = "100원 이상 결제하셔야 합니다.") int totalAmount,
+        @NotNull PaymentMethod paymentMethod,
         @NotBlank String cardCompany,
         int installmentMonth
 ) {
+    public static PaymentRequest from(Order order, Long memberId, OrderRequest request) {
+        return new PaymentRequest(
+                order.getId(),
+                memberId,
+                order.getTotalPrice(),
+                PaymentMethod.valueOf(request.paymentMethod()),
+                request.cardCompany(),
+                request.installmentMonth()
+        );
+    }
 }
