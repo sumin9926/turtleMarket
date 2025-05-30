148a6736e27187b9ee822a6c7350675bf1132638
임PRO스터\su992 <su9926@naver.com>
2025-05-30 15:41:40 +0900

    feat: 환불 승인 기능 구현

--- src/main/java/turtleMart/order/controller/RefundController.java
+++ src/main/java/turtleMart/order/controller/RefundController.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.async.DeferredResult;
 import turtleMart.order.dto.response.RefundApplyResultResponse;
 import turtleMart.order.dto.response.RefundResponse;
 import turtleMart.order.service.RefundService;
+import turtleMart.order.service.RefundWaiter;
 
 import java.util.List;
 
@@ -16,6 +18,7 @@ import java.util.List;
 public class RefundController {
 
     private final RefundService refundService;
+    private final RefundWaiter refundWaiter;
 
     @PostMapping("/{orderItemId}")
     public ResponseEntity<RefundApplyResultResponse> applyRefund(
@@ -35,5 +38,16 @@ public class RefundController {
         List<RefundResponse> responseList = refundService.getRefundRequestList(1L);
 
         return ResponseEntity.status(HttpStatus.OK).body(responseList);
+    }
+
+    @PatchMapping("/{orderItemId}")
+    public DeferredResult<ResponseEntity<Void>> approveRefund(
+            @PathVariable Long orderItemId
+            /*TODO 토큰에서 판매자 ID 가져오기*/
+    ){
+        DeferredResult<ResponseEntity<Void>> result = refundWaiter.createWaiter(orderItemId);
+        refundService.approveOrderItemRefund(orderItemId, 1L);
+
+        return result;
     }
 }
