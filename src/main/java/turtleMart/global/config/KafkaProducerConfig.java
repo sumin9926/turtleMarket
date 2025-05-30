3dfdb50ca915903acbd61c26e7f7f47e09b21420
tmdcksdl <tmdcksdl99@naver.com>
2025-05-30 15:49:19 +0900

    feat: KafkaProducerConfig에 설정 추가

--- src/main/java/turtleMart/global/config/KafkaProducerConfig.java
+++ src/main/java/turtleMart/global/config/KafkaProducerConfig.java
@@ -19,7 +19,12 @@ public class KafkaProducerConfig {
 
     @Bean
     public ProducerFactory<String, Object> producerFactoryForObject() {
-        return new DefaultKafkaProducerFactory<>(new HashMap<>());
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(configProps);
     }
 
     @Bean
