f1be96ab0893fda05bbcb074a83493e2ecd07fea
임PRO스터\su992 <su9926@naver.com>
2025-05-30 11:31:33 +0900

    feat: OrderId 업데이트 메서드 추가

--- src/main/java/turtleMart/payment/dto/request/PaymentRequest.java
+++ src/main/java/turtleMart/payment/dto/request/PaymentRequest.java
@@ -6,11 +6,21 @@ import jakarta.validation.constraints.NotNull;
 import turtleMart.payment.entity.PaymentMethod;
 
 public record PaymentRequest(
-        @NotNull Long orderId,
+        Long orderId, //컨트롤러에서는 null 로 받기, Service 에서 채워줄 예정
         @NotNull Long memberId,
         @Min(value = 100, message = "100원 이상 결제하셔야 합니다.") int amount,
         @NotNull PaymentMethod paymentMethod,
         @NotBlank String cardCompany,
         int installmentMonth
 ) {
+    public static PaymentRequest updateOrderId(PaymentRequest paymentRequest, Long orderId){
+        return new PaymentRequest(
+                orderId,
+                paymentRequest.memberId(),
+                paymentRequest.amount(),
+                paymentRequest.paymentMethod(),
+                paymentRequest.cardCompany(),
+                paymentRequest.installmentMonth()
+        );
+    }
 }
