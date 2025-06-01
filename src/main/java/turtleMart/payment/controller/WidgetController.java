6a62bf80737c28d49c816d3b23ff254726005916
Happy Beny <qkdehdgur3@gmail.com>
2025-06-02 02:19:46 +0900

    refactor : 결제 정상화(orderId는 문자열 6글자 이상, amount는 숫자로, 나머지는 전체 문자로. String로 넘겨준다면 [[#{}]]를 따옴표로 안 감싸줘도 됨)

--- src/main/java/turtleMart/payment/controller/WidgetController.java
+++ src/main/java/turtleMart/payment/controller/WidgetController.java
@@ -94,7 +94,7 @@ public class WidgetController {
 
         // 토스페이먼츠 API는 시크릿 키를 사용자 ID로 사용하고, 비밀번호는 사용하지 않습니다.
         // 비밀번호가 없다는 것을 알리기 위해 시크릿 키 뒤에 콜론을 추가합니다.
-        String widgetSecretKey = "test_gck_docs_Ovk5rk1EwkEbP0W43n07xlzm";
+        String widgetSecretKey = "test_gsk_docs_OaPz8L5KdmQXkzRz3y47BMw6";
         Base64.Encoder encoder = Base64.getEncoder();
         byte[] encodedBytes = encoder.encode((widgetSecretKey + ":").getBytes(StandardCharsets.UTF_8));
         String authorizations = "Basic " + new String(encodedBytes);
--- src/main/java/turtleMart/payment/dto/PaymentInfoTransfer.java
+++ src/main/java/turtleMart/payment/dto/PaymentInfoTransfer.java
@@ -3,7 +3,7 @@ package turtleMart.payment.dto;
 import turtleMart.order.entity.Order;
 
 public record PaymentInfoTransfer(
-        Long orderId,
+        String orderId,
         Integer totalAmount,
         String orderName,
         String customerEmail,
@@ -11,12 +11,13 @@ public record PaymentInfoTransfer(
         String customerPhoneNumber
 ) {
     public static PaymentInfoTransfer from(Order order){
+        String orderId = "order_" + order.getId();
         String orderName = order.getOrderItems().get(0).getName();
         if (order.getOrderItems().size() != 1) {
             orderName += " 외 " + (order.getOrderItems().size() - 1) + "개 상품";
         }
         return new PaymentInfoTransfer(
-                order.getId(),
+                orderId,
                 order.getTotalPrice(),
                 orderName,
                 order.getMember().getEmail(),
