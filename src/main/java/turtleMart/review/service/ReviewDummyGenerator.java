4875a92f00b946870864d48a6f07d7ddc773ef6c
lee sujin <jin18302@naver.com>
2025-06-02 11:05:35 +0900

    feat: 리뷰 더미데이터 생성

--- src/main/java/turtleMart/review/service/ReviewDummyGenerator.java
+++ src/main/java/turtleMart/review/service/ReviewDummyGenerator.java
@@ -5,9 +5,13 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import turtleMart.member.entity.Member;
 import turtleMart.member.repository.MemberRepository;
+import turtleMart.order.entity.Order;
 import turtleMart.order.entity.OrderItem;
 import turtleMart.order.repository.OrderItemRepository;
+import turtleMart.order.repository.OrderRepository;
 import turtleMart.product.entity.Product;
+import turtleMart.product.entity.ProductOptionCombination;
+import turtleMart.product.repository.ProductOptionCombinationRepository;
 import turtleMart.review.entity.Review;
 import turtleMart.review.repository.ReviewRepository;
 
@@ -21,8 +25,9 @@ public class ReviewDummyGenerator {
 
     private final ReviewRepository reviewRepository;
     private final MemberRepository memberRepository;
+    private final ProductOptionCombinationRepository productOptionCombinationRepository;
 //    private final Faker faker = new Faker(new Locale("ko")); // 한글 Faker
-    private final Random random = new Random();
+    public static Random random = new Random();
 
     private final String[] titles = {
             "완전 만족해요", "배송 빠르고 좋아요", "별로였어요", "재구매 의사 있습니다", "가격 대비 괜찮아요",
@@ -86,15 +91,57 @@ public class ReviewDummyGenerator {
         );
     }
 
-    private <T> T pick(T[] arr) {
+    public static <T> T pick(T[] arr) {
         return arr[random.nextInt(arr.length)];
     }
+
 }
 @Component
+@RequiredArgsConstructor
 class OrderItemGenerator{
 
-    public void generatorOrderItem(){
+    private final OrderRepository orderRepository;
+    private final ProductOptionCombinationRepository productOptionCombinationRepository;
+    private final OrderItemRepository orderItemRepository;
 
+
+
+
+   Integer[] priceList = {325810, 45244, 149020, 272389, 394886, 111188, 339242, 132583, 479384, 125717,
+                    215254, 45626, 270666, 153739, 184857, 467939, 464812, 271899, 320346, 182237,
+                    306586, 352124, 482582, 303800, 208792, 213661, 476202, 165980, 327369, 200849};
+
+String[]itemName = {
+        "슈퍼 가방", "스마트 모니터", "프리미엄 모니터", "스마트 스니커즈", "에코 노트북",
+            "클래식 모니터", "스마트 핸드폰", "에코 시계", "클래식 노트북", "뉴 모니터",
+            "클래식 책상", "에코 책상", "프리미엄 핸드폰", "에코 핸드폰", "에센셜 책상",
+            "클래식 이어폰", "슈퍼 핸드폰", "프리미엄 가방", "프리미엄 이어폰", "프리미엄 스니커즈",
+            "슈퍼 이어폰", "뉴 이어폰", "에코 책상", "프리미엄 책상", "뉴 노트북",
+            "라이트 이어폰", "뉴 선풍기", "프리미엄 노트북", "슈퍼 핸드폰", "슈퍼 의자",
+            "스마트 가방", "스마트 노트북", "클래식 이어폰", "클래식 핸드폰", "슈퍼 책상",
+            "프리미엄 선풍기", "프리미엄 이어폰", "라이트 가방", "프리미엄 가방", "라이트 이어폰",
+            "라이트 시계", "스마트 이어폰", "스마트 스니커즈", "라이트 책상", "프리미엄 책상",
+            "뉴 이어폰", "에코 이어폰", "스마트 가방", "슈퍼 이어폰", "뉴 스니커즈"
+};
+
+    Integer[] quantity = {1,5,6,2,22,4,3,14,8};
+
+
+
+
+    public void generatorOrderItem(int repeat){
+
+        Order order = orderRepository.getReferenceById(1L);
+        ProductOptionCombination productOptionCombination = productOptionCombinationRepository.getReferenceById(1L);
+        ArrayList<OrderItem> orderItems = new ArrayList<>();
+
+        for(int i = 0 ; i < repeat ; i++){
+            OrderItem  orderItem =  OrderItem.of(order, productOptionCombination, ReviewDummyGenerator.pick(priceList),
+                    ReviewDummyGenerator.pick(itemName) ,  ReviewDummyGenerator.pick(quantity));
+
+            orderItems.add(orderItem);
+        }
+
+        orderItemRepository.saveAll(orderItems);
     }
-
 }
