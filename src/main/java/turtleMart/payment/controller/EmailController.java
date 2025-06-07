ea729ffd1cb971022693c3bfc23335cba103f94d
Happy Beny <qkdehdgur3@gmail.com>
2025-06-08 03:10:46 +0900

    feat : 메일 발송 최소단위 기능 구현

--- src/main/java/turtleMart/payment/controller/EmailController.java
+++ src/main/java/turtleMart/payment/controller/EmailController.java
@@ -6,17 +6,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import turtleMart.payment.dto.request.EmailRequest;
 import turtleMart.payment.service.EmailService;
 
 @RestController
-@RequestMapping("/webhook")
+@RequestMapping("/emails")
 @RequiredArgsConstructor
 public class EmailController {
 
     private final EmailService emailService;
 
-    @PostMapping("/toss-payment")
-    public ResponseEntity<Void> handleTossWebhook(@RequestBody String payload) {
+    @PostMapping("/payment-confirmation")
+    public ResponseEntity<Void> sendPaymentConfirmationEmail(@RequestBody EmailRequest request) {
+        emailService.sendPaymentCompleteEmail(request.to(), request.subject(), request.content());
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/turtleMart/payment/service/EmailService.java
+++ src/main/java/turtleMart/payment/service/EmailService.java
@@ -2,10 +2,19 @@ package turtleMart.payment.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
-//    private final MailSender mailSender;
+    private final MailSender mailSender;
+
+    public void sendPaymentCompleteEmail(String to, String subject, String content) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(content);
+        mailSender.send(message);
+    }
 }
