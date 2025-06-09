63e1c9bd12b9067792afdcd7f26385f951278c85
임PRO스터\su992 <su9926@naver.com>
2025-06-09 17:00:59 +0900

    feat: 주문 생성 Kafka 리스너에 예외 처리 로직 추가
    - NotFoundException 처리 시 재처리하지 않도록 로그만 기록

--- src/main/java/turtleMart/global/kafka/listener/OrderKafkaListener.java
+++ src/main/java/turtleMart/global/kafka/listener/OrderKafkaListener.java
@@ -1,15 +1,18 @@
 package turtleMart.global.kafka.listener;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
+import turtleMart.global.exception.NotFoundException;
 import turtleMart.global.kafka.dto.OperationWrapperDto;
 import turtleMart.global.utill.JsonHelper;
 import turtleMart.order.dto.request.OrderWrapperRequest;
 import turtleMart.order.service.OrderService;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class OrderKafkaListener {
@@ -17,13 +20,19 @@ public class OrderKafkaListener {
     private final OrderService orderService;
 
     @KafkaListener(topics = "order_create_topic", groupId = "order-group")
-    public void listenOrderCreate(@Header(KafkaHeaders.RECEIVED_KEY) String key, String value){
+    public void listenOrderCreate(@Header(KafkaHeaders.RECEIVED_KEY) String key, String value) {
+
         OperationWrapperDto wrapperDto = JsonHelper.fromJson(value, OperationWrapperDto.class);
         String payload = wrapperDto.payload();
         OrderWrapperRequest orderWrapperRequest = JsonHelper.fromJson(payload, OrderWrapperRequest.class);
-        // TODO 파싱, 서비스 호출 안 됐을 때 try-catch 예외 추가, 로그 추가 혹은 재시도 전략 고민
 
-
-        orderService.createOrder(orderWrapperRequest);
+        try {
+            orderService.createOrder(orderWrapperRequest);//익셉션 캐치해서 로그 찍기
+        } catch (NotFoundException e) {
+            log.error("잘못된 주문 요청 - 필수 데이터 없음(재처리 x). key={}, message={}", key, e.getMessage());
+            // 필수 정보 누락으로 인한 예외라 재처리 무의미, 타임아웃 될 예정
+        } catch (Exception e) {
+            log.error("Kafka 주문 생성 처리 중 예기치 못한 오류 발생. key={}, value={}", key, value, e);
+        }
     }
 }
