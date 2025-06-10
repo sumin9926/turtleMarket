e4df176f71272206c63eb915c4a9f5a6da455b5e
Happy Beny <qkdehdgur3@gmail.com>
2025-06-10 15:29:39 +0900

    refactor : payment, order JOIN FETCH

--- src/main/java/turtleMart/payment/repository/PaymentRepository.java
+++ src/main/java/turtleMart/payment/repository/PaymentRepository.java
@@ -1,6 +1,8 @@
 package turtleMart.payment.repository;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import turtleMart.payment.entity.Payment;
 
 import java.util.List;
@@ -9,5 +11,10 @@ import java.util.Optional;
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     Optional<List<Payment>> findAllByMemberId(Long memberId);
 
-    Optional<Payment> findByOrderId(Long orderId);
+    @Query("""
+SELECT p
+FROM Payment p JOIN FETCH p.order o JOIN FETCH o.member m
+WHERE p.order.id=:orderId
+""")
+    Optional<Payment> findByOrderId(@Param("orderId") Long orderId);
 }
