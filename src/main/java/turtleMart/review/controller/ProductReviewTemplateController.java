26e4c373a141f6167ec9553a91487f44fa696981
lee sujin <jin18302@naver.com>
2025-06-12 12:09:57 +0900

    chore: 잘못된 임포트 제거

--- src/main/java/turtleMart/review/controller/ProductReviewTemplateController.java
+++ src/main/java/turtleMart/review/controller/ProductReviewTemplateController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import turtleMart.review.dto.request.CreateProductReviewTemplateRequest;
-import turtleMart.review.dto.request.UpdateProductReviewTemplateRequest;
 import turtleMart.review.dto.response.ProductReviewTemplateResponse;
 import turtleMart.review.service.ProductReviewTemplateService;
 import turtleMart.security.AuthUser;
