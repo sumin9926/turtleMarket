862e48a99b0f15e84e8794ad875f551ff759a22c
임PRO스터\su992 <su9926@naver.com>
2025-06-13 09:41:58 +0900

    fix: 카프카 토픽 이름 수정 및 역직렬화 설정 추가

--- src/main/java/turtleMart/global/kafka/config/KafkaProducerConfig.java
+++ src/main/java/turtleMart/global/kafka/config/KafkaProducerConfig.java
@@ -24,7 +24,11 @@ public class KafkaProducerConfig {
 
     @Bean
     public ProducerFactory<String, String> producerFactoryForString() {
-        return new DefaultKafkaProducerFactory<>(new HashMap<>());
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
     }
 
     @Bean
